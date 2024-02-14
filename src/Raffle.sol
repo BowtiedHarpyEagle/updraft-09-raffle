b9dd1802bd36b49105a439bbe64fd14d8e1e6c44
Bowtied HarpyEagle <140409172+BowtiedHarpyEagle@users.noreply.github.com>
2024-02-14 08:24:28 -0500

    complete L9P01

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -9,4 +9,24 @@ pragma solidity  ^0.8.18;
  * @dev Implements Chainlink VRFv2 for random number generation
  */
 
-contract Raffle {}
+contract Raffle {
+    uint256 private immutable i_entranceFee;
+
+    constructor(uint256 _entranceFee) {
+        i_entranceFee = _entranceFee;
+    }
+
+    function enterRaffle()  public payable{
+        
+    }
+
+    function pickWinner() public {
+        
+    }
+
+    /** Getter functions */
+
+    function getEntranceFee() public view returns(uint256) {
+        return i_entranceFee;
+    }
+}
