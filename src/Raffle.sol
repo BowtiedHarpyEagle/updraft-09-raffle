e597fdb20436557aa4b7ffc3252d907bfa98ab10
Bowtied HarpyEagle <140409172+BowtiedHarpyEagle@users.noreply.github.com>
2024-02-14 08:10:59 -0500

    L9P1 9:20 start add comments for context

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,4 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity   ^0.8.18;
 
+//**
+ * @title A sample raffle contract 
+ * @author Patrick Collins - Foundry full course 2023 - BowtiedHarpyEagle going through the course
+ * @notice This contract is for creating a provably fair lottery
+ * @dev Implements Chainlink VRFv2 for random number generation
+ */
+
 contract Raffle {}
