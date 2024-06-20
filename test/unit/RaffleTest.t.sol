cd8dd72d481fa0e041be3ce7da08bc5f6160994e
BowtiedHarpyEagle <140409172+BowtiedHarpyEagle@users.noreply.github.com>
2024-06-20 08:49:18 -0500

    complete L9P20

--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -7,6 +7,10 @@ import {Test, console} from "forge-std/Test.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
 
 contract RaffleTest is Test {
+    /* Events */ 
+
+    event EnteredRaffle(address indexed player);
+
     Raffle raffle;
     HelperConfig helperConfig;
 
@@ -54,6 +58,13 @@ contract RaffleTest is Test {
         raffle.enterRaffle{value: entranceFee}();
         address playerRecorded = raffle.getPlayer(0);
         assert(playerRecorded == PLAYER);
+    }
+
+    function testEmitsEventOnEntery() public {
+        vm.prank(PLAYER);
+        vm.expectEmit(true,false,false,false,address(raffle));
+        emit EnteredRaffle(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
     }
 
 
